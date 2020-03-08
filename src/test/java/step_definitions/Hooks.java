b45854b053cad86a66978ef70e681387f67aff4e
dmytrochernetskyi <directusa2012@gmail.com>
2020-03-08 12:43:55 -0500

    added hooks

---
+++ src/test/java/step_definitions/Hooks.java
@@ -0,0 +1,32 @@
+package step_definitions;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.Driver;
+import utilities.Log;
+import utilities.TempStorage;
+
+public class Hooks {
+
+    @Before
+    public void setUp(Scenario scenario){
+        //anything that you want to run before each Scenario
+        TempStorage.clear();
+        Log.start(scenario.getName());
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()) {
+            byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+
+
+        Driver.quitDriver();
+
+    }
+}
